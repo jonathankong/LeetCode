8745e0c7d1aacd5d43a6ac97340cb123637aaa18
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-08-24 00:49:57 -0400

    Project updates

---
+++ LeetCodeSolutions/LeetCodeMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    class LeetCodeMain
+    {
+        static void Main(string[] args)
+        {
+            //Two Sum
+            //Console.WriteLine($"[{string.Join(", ", TwoSum.Solution(new int[] { 3, 2, 4 }, 6))}]");
+
+            //Add Two Numbers
+
+            //Create L1
+            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+            Console.WriteLine(AddTwoNumbers.Solution(l1, l2));
+        }
+    }
+}
