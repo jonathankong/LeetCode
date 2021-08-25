ec1ae89d0457ffd22e4aff052c7f130d1ac49cd6
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-08-24 23:28:26 -0400

    Update LeetCodeMain.cs

--- LeetCodeSolutions/LeetCodeMain.cs
+++ LeetCodeSolutions/LeetCodeMain.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace LeetCodeSolutions
 {
@@ -11,11 +14,12 @@ namespace LeetCodeSolutions
             //Console.WriteLine($"[{string.Join(", ", TwoSum.Solution(new int[] { 3, 2, 4 }, 6))}]");
 
             //Add Two Numbers
+            //ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            //ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+            //Console.WriteLine(AddTwoNumbers.Solution(l1, l2));
 
-            //Create L1
-            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-            Console.WriteLine(AddTwoNumbers.Solution(l1, l2));
+            //Longest Substring Without Repeating Characters
+            Console.WriteLine(LongestSubstringWithoutRepeatChars.Solution("Hello"));
         }
     }
 }
