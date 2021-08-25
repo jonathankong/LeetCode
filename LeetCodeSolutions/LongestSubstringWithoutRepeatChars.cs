e89c45562f43636b8b42d8356ea557351acc3e0a
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-08-24 23:28:14 -0400

    Create LongestSubstringWithoutRepeatChars.cs
    Using a queue to keep track of the longest SubString with unique characters

---
+++ LeetCodeSolutions/LongestSubstringWithoutRepeatChars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    static class LongestSubstringWithoutRepeatChars
+    {
+        public static int Solution(string s)
+        {
+            int longestSubStringLength = 0;
+            Queue<char> tempSubString = new Queue<char>();
+
+            foreach (char c in s)
+            {
+                if (tempSubString.Contains(c) && tempSubString.Count > longestSubStringLength)
+                {
+                    longestSubStringLength = tempSubString.Count;
+                }
+                while (tempSubString.Contains(c))
+                {
+                    tempSubString.Dequeue();
+                }
+                tempSubString.Enqueue(c);
+            }
+
+            return Math.Max(tempSubString.Count, longestSubStringLength);
+        }
+    }
+}
