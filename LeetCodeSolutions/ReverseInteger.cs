e73d44f97ecf799295b0e46cf49d4ed2124e6254
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-08-29 16:57:39 -0400

    Reverse Integer not working

---
+++ LeetCodeSolutions/ReverseInteger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    static class ReverseInteger
+    {
+        public static int Solution(int x)
+        {
+
+            int length = Math.Abs(x).ToString().Length;
+
+            int answer = 0;
+
+
+            for (int i = 1; i <= length; i++)
+            {
+
+
+                int tempDigit = x % (int)Math.Pow(10, i) / (int)Math.Pow(10, i - 1);
+
+                if (tempDigit * Math.Pow(10, length - i) <= Math.Pow(-2, 31) || tempDigit * Math.Pow(10, length - i) > Math.Pow(2, 31) - 1)
+                {
+                    answer = 0;
+                    break;
+                }
+
+                answer = answer + tempDigit * (int)Math.Pow(10, length - i);
+            }
+
+            return (int)answer;
+        }
+    }
+}
