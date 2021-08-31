3418cca1d3be34b5750b1cb96ba76ef4ad7f2a62
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-08-30 22:44:12 -0400

    Concat Arrays

---
+++ LeetCodeSolutions/ContatenationOfArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    static class ContatenationOfArray
+    {
+        //First Solution
+
+        static int[] Solution(int[] nums)
+        {
+            List<int> ans = new List<int>();
+            for (int counter = 1; counter <= 2; counter++)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    ans.Add(nums[i]);
+                }
+            }
+            return ans.ToArray();
+        }
+
+        //Second Solution
+
+        //static int[] Solution(int[] nums)
+        //{
+        //    List<int> ans = new List<int>(nums);
+
+        //    ans.AddRange(nums);
+
+        //    return ans.ToArray();
+        //}
+    }
+}
