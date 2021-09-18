308f1a7d1dbe341022703922b36f8066205626eb
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-09-18 14:15:49 -0400

    Create PhoneNumberLetterCombinations.cs
    Incomplete

---
+++ LeetCodeSolutions/PhoneNumberLetterCombinations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    public static class PhoneNumberLetterCombinations
+    {
+        public static IList<string> LetterCombinations(string digits)
+        {
+            IList<string> answers = new Queue<string>() { "" };
+            if (digits.Length == 0) return answers;
+
+            String[] letterGroupings = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+
+            foreach(char c in digits)
+            {
+                var tempChar = answers.Remove();
+            }
+        }
+    }
+}
