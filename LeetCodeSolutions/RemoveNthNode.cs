6eda1f560edeaa6d2f51f0fd65aa3e461f978bf5
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-09-18 14:15:58 -0400

    Create RemoveNthNode.cs
    Working solution

---
+++ LeetCodeSolutions/RemoveNthNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    static class RemoveNthNode
+    {
+        public static ListNode Solution(ListNode head, int n)
+        {
+            int listNodeLength = 1;
+            var currentNode = head;
+
+            //Get length of linked list first
+            while (currentNode.next != null)
+            {
+                currentNode = currentNode.next;
+                listNodeLength++;
+            }
+            //Reset currentNode back to head of linked list
+            currentNode = head;
+
+            //Return next in linked list if head is going to be removed
+            if (listNodeLength - n == 0) return head.next;
+            else
+            {
+                for (int i = 1; i < listNodeLength - n; i++)
+                {
+                    currentNode = currentNode.next;
+                }
+
+                currentNode.next = currentNode.next.next;
+            }
+            return head;
+        }
+    }
+}
