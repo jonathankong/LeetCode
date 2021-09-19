a95bd5a0ef1575e23b69157d5b528079425b7f19
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-09-19 13:32:01 -0400

    Create ValidParentheses.cs

---
+++ LeetCodeSolutions/ValidParentheses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    static class ValidParentheses
+    {
+        public static bool Solution(string s)
+        {
+            if (s.Length % 2 == 1) return false;
+            Stack<char> charStack = new Stack<char>();
+
+            foreach(char c in s)
+            {
+                if (charStack.Count == 0 || c == '(' || c == '[' || c == '{')
+                {
+                    charStack.Push(c);
+                }
+                else if (charStack.Peek() == '(' && c == ')' ||
+                    charStack.Peek() == '{' && c == '}' ||
+                    charStack.Peek() == '[' && c == ']')
+                {
+                    charStack.Pop();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return charStack.Count == 0;
+        }
+    }
+}
