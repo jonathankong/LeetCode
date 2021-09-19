1ec7874c7734da6e54ad8721ab63aa908cef2c9d
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-09-19 13:31:55 -0400

    Create GenerateParenthesis.cs

---
+++ LeetCodeSolutions/GenerateParenthesis.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+
+    static class GenerateParenthesis
+    {
+        static IDictionary<int, char> chars = new Dictionary<int, char>()
+        {
+            { 1, '(' },
+            { 2, ')' }
+        };
+
+        public static IList<string> Solution(int n)
+        {
+            IList<string> answers = new List<string>();
+            Recursion(answers, n);
+            
+            return answers;
+        }
+
+        private static void Recursion(IList<string> answers, int n)
+        {
+            Recursion(answers, "", n, n);
+        }
+
+        private static void Recursion(IList<string> answers, string str, int openBrackets, int closeBrackets)
+        {
+            char lastChar = '\0';
+            if (str.Length > 0)
+            {
+                lastChar = str[str.Length - 1];
+            }
+            //End recursion statement
+            if (openBrackets == 0 && closeBrackets == 0)
+            {
+                answers.Add(str.ToString());
+                return;
+            }
+            else if (str.Length == 0 || lastChar == chars[2] && openBrackets == closeBrackets)
+            {
+                //Balance pairs to be made
+                Recursion(answers, str + chars[1], openBrackets - 1, closeBrackets);
+            }
+            //If last character is an open character and there are more pairs left to create
+            //or there are unbalanced pairs left in the string
+            else if ((lastChar == chars[1] || openBrackets < closeBrackets) && openBrackets > 0)
+            {
+                Recursion(answers, str + chars[1], openBrackets - 1, closeBrackets);
+                Recursion(answers, str + chars[2], openBrackets, closeBrackets - 1);
+            }
+            else
+            {
+                Recursion(answers, str + chars[2], openBrackets, closeBrackets - 1);
+            }
+        }
+    }
+}
