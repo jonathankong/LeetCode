46e3cf1eca4d198788782d8fa8951aba75ede870
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-09-19 13:31:58 -0400

    Create MergeTwoSortedLists.cs

---
+++ LeetCodeSolutions/MergeTwoSortedLists.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    static class MergeTwoSortedLists
+    {
+        public static ListNode Solution(ListNode l1, ListNode l2)
+        {
+            if (l1 == null && l2 == null) return null;
+
+            var answer = new ListNode();
+            var currentNode = answer;
+
+            while (l1 != null || l2 != null)
+            {
+                if (l2 == null || l1 != null && l1.val <= l2.val)
+                {
+                    currentNode.val = l1.val;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    currentNode.val = l2.val;
+                    l2 = l2.next;
+                }
+
+                if (l1 != null || l2 != null)
+                {
+                    currentNode.next = new ListNode();
+                    currentNode = currentNode.next;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
