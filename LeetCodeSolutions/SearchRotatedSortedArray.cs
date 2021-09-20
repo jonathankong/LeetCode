43c94c07cb5cc2f364dcff2648d7dd49267927e8
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-09-20 18:47:11 -0400

    Create SearchRotatedSortedArray.cs

---
+++ LeetCodeSolutions/SearchRotatedSortedArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    static class SearchRotatedSortedArray
+    {
+        #region First Solution
+        //public static int Solution (int[] nums, int target)
+        //{
+        //    if (nums[0] <= target)
+        //    {
+        //        for (int i = 0; i < nums.Length; i++)
+        //        {
+        //            if (nums[i] == target)
+        //            {
+        //                return i;
+        //            }
+        //            else if (i + 1 < nums.Length && nums[i] > nums[i + 1])
+        //            {
+        //                return -1;
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        for (int i = nums.Length - 1; i >= 0; i--)
+        //        {
+        //            if (nums[i] == target)
+        //            {
+        //                return i;
+        //            }
+        //            else if (i - 1 >= 0 && nums[i] < nums[i - 1])
+        //            {
+        //                return -1;
+        //            }
+        //        }
+        //    }
+        //    return -1;
+        //}
+        #endregion
+
+        #region Second Solution using Binary Search methodology
+        //public static int Solution (int[] nums, int target)
+        //{
+        //    int l = 0;
+        //    int r = nums.Length - 1;
+
+        //    while (l <= r)
+        //    {
+        //        var mid = (l + r) / 2;
+
+                
+        //    }
+        //}
+        #endregion
+    }
+}
