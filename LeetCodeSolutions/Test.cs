2e01ca7de578041ad3f323159a5df3a023732031
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-10-16 16:37:37 -0400

    Create Test.cs
    Just a file to test questions from Cracking the Coding Interview

---
+++ LeetCodeSolutions/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    static class Test
+    {
+        public static int Solution(int steps)
+        {
+            int answer = 0;
+            int step1 = 1;
+            int step2 = 1;
+            int step3 = 2;
+
+            for (int i = 0; i < steps - 2; i++)
+            {
+                var temp1 = step1;
+                var temp2 = step2;
+                step1 = step2;
+                step2 = step3;
+                step3 = temp1 + temp2 + step2;
+            }
+
+            return step3;
+        }
+    }
+}
