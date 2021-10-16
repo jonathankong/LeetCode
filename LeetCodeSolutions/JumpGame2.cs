b7ad9d82ca689103035490f4b91b399180e15a2d
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-10-16 16:37:17 -0400

    Create JumpGame2.cs
    Not complete

---
+++ LeetCodeSolutions/JumpGame2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    static class JumpGame2
+    {
+        public static int Solution(int[] nums)
+        {
+            if (nums.Length == 1 && nums[0] == 0) return 0;
+            else if (nums.Length == 1 && nums[0] != 0 && nums.Length == 2) return 1;
+            else return Recursion(nums);
+        }
+
+        private static int Recursion(int[] nums)
+        {
+            if (nums.Length == 1) return 1;
+            else if (nums.Length == 2) return 1;
+            int value = -1;
+            for (int i = nums.Length - 2; i >= 0; i--)
+            { 
+                if ((nums.Length - 1) - i <= nums[i])
+                {
+                    value = nums[i];
+                }
+            }
+            return value == -1 ? 1 : 1 + Recursion(nums.Take(nums.Length - value).ToArray());
+        }
+    }
+}
