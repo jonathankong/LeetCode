07c8f75ab70c4269e976cb49047c56bab67185d0
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-11-02 17:38:20 -0400

    Create MergeIntervals.cs
    Not complete

---
+++ LeetCodeSolutions/MergeIntervals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    static class MergeIntervals
+    {
+        public static int[][] Solution (int[][] intervals)
+        {
+            IList<int[]> answers = new List<int[]>();
+            int[] tempAnswer = intervals[0];
+            int counter = 1;
+
+            while (counter < intervals.Length)
+            {
+                if (intervals[counter][0] <= tempAnswer[0] && intervals[counter][1] >= tempAnswer[1])
+                {
+                    tempAnswer = intervals[counter];
+                }
+                else if (intervals[counter][0] <= tempAnswer[1])
+                {
+                    tempAnswer[1] = intervals[counter][1];
+                }
+                else if (intervals[counter][0] <= tempAnswer[0])
+                {
+                    tempAnswer[0] = intervals[counter][0];
+                }
+                else
+                {
+                    answers.Add(tempAnswer);
+                    tempAnswer = intervals[counter];
+                }
+                counter++;
+            }
+            answers.Add(tempAnswer);
+            return answers.ToArray();
+        }
+    }
+}
