de3086782c51e6ad41d7ef68370a164925928414
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-11-03 22:11:08 -0400

    Update LeetCodeMain.cs

--- LeetCodeSolutions/LeetCodeMain.cs
+++ LeetCodeSolutions/LeetCodeMain.cs
@@ -101,13 +101,27 @@ namespace LeetCodeSolutions
             //var s = GroupAnagrams.Solution(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
 
             //Merge Intervals
-            var s = MergeIntervals.Solution(new int[][] {
-                new int[] { 1, 4 }
-                //new int[] { 1, 3 }, 
-                //new int[] { 2, 6 },
-                //new int[] { 8, 10 },
-                //new int[] { 15, 18 }
-            });
+            //var s = MergeIntervals.Solution(new int[][] {
+            //    new int[] { 1, 4 }
+            //    //new int[] { 1, 3 }, 
+            //    //new int[] { 2, 6 },
+            //    //new int[] { 8, 10 },
+            //    //new int[] { 15, 18 }
+            //});
+
+            //Unique Paths
+            //Console.WriteLine(UniquePaths.Solution(3, 2));
+
+            //Minimum Path Sum
+            Console.WriteLine(MinimumPathSum.Solution(new int[][] {
+                new int[] { 1, 4, 8, 6, 2, 2, 1, 7 },
+                new int[] { 4, 7, 3, 1, 4, 5, 5, 1 },
+                new int[] { 8, 8, 2, 1, 1, 8, 0, 1 },
+                new int[] { 8, 9, 2, 9, 8, 0, 8, 9 },
+                new int[] { 5, 7, 5, 7, 1, 8, 5, 5 },
+                new int[] { 7, 0, 9, 4, 5, 6, 5, 6 },
+                new int[] { 4, 9, 9, 7, 9, 1, 9, 0 }
+            }));
         }
     }
 }
