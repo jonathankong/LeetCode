6bc374d25df365bc1c9a652ae8d11e35c029a1a1
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-11-03 23:12:26 -0400

    Update MinimumPathSum.cs
    Correct answer using same grid.  Solution is O(m*n) and size O(1)

--- LeetCodeSolutions/MinimumPathSum.cs
+++ LeetCodeSolutions/MinimumPathSum.cs
@@ -9,47 +9,31 @@ namespace LeetCodeSolutions
         //Using Bottom Up approach
         static public int Solution(int[][] grid)
         {
-            return Recursion(grid, grid.Length - 1, grid[0].Length - 1);
-        }
-
-        static private int Recursion(int[][] grid, int startX, int startY)
-        {
-            if (startX == 0 && startY == 0)
+            for (int i = grid.Length - 1; i >= 0; i--)
             {
-                return grid[startX][startY];
-            }
-            else if (startX < 0 || startY < 0)
-            {
-                return 0;
-            }
-
-            int startXminus1 = startX == 0 ? startX : startX - 1;
-            int startYminus1 = startY == 0 ? startY : startY - 1;
-            //Compare Left vs Up option
-            //If left is bigger than up, go up
-            if (grid[startX][startYminus1] > grid[startXminus1][startY])
-            {
-                //If at top most of array, must go left
-                if (startX == 0)
+                for (int j = grid[0].Length - 1; j >= 0; j--)
                 {
-                    return grid[startX][startY] + Recursion(grid, startX, startYminus1);
+                    //Add minimum of bottom or right of cell to cell
+                    //Checks for out of bounds
+                    if (i + 1 == grid.Length && j + 1 == grid[0].Length)
+                    {
+                        continue;
+                    }
+                    else if (i + 1 == grid.Length)
+                    {
+                        grid[i][j] += grid[i][j + 1];
+                    }
+                    else if (j + 1 == grid[0].Length)
+                    {
+                        grid[i][j] += grid[i + 1][j];
+                    }
+                    else
+                    {
+                        grid[i][j] += Math.Min(grid[i + 1][j], grid[i][j + 1]);
+                    }
                 }
-                return grid[startX][startY] + Recursion(grid, startXminus1, startY);
             }
-            else if (grid[startX][startYminus1] < grid[startXminus1][startY])
-            {
-                //If at left most of array, must go up
-                if (startY == 0)
-                {
-                    return grid[startX][startY] + Recursion(grid, startXminus1, startY);
-                }
-                return grid[startX][startY] + Recursion(grid, startX, startYminus1);
-            }
-            //If equal
-            else
-            {
-                return 
-            }
+            return grid[0][0];
         }
     }
 }
