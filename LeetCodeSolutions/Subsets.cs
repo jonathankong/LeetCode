9d2afc21fbee6cd8c406887c9eebde0381a72aaf
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-11-12 18:09:58 -0500

    Create Subsets.cs
    Not complete

---
+++ LeetCodeSolutions/Subsets.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    public static class Subsets
+    {
+        public static IList<IList<int>> Solution(int[] nums)
+        {
+            var answersList = new List<IList<int>>();
+            answersList.Add(new List<int>());
+
+            //Grab each element of the array
+            for(int i = 0; i < nums.Length; i++)
+            {
+                //This loop will increase the size of the subset array 
+                for(int j = 1; j <= nums.Length - i; j++)
+                {
+                    answersList.Add(nums.Skip(i).Take(j).ToList());
+                }
+            }
+            return answersList;
+        }
+    }
+}
