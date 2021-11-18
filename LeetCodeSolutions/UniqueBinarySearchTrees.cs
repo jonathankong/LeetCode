bde125f1aac98b107efc33276f9507cf89dac6a9
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-11-17 20:10:40 -0500

    Create UniqueBinarySearchTrees.cs
    Not complete

---
+++ LeetCodeSolutions/UniqueBinarySearchTrees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    public static class UniqueBinarySearchTrees
+    {
+        public static int Solution(int n)
+        {
+            //I need to do more research on Catalan Numbers and Combinatorics.
+            return 0;
+        }
+    }
+}
