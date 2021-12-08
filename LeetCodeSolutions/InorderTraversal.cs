1d20e2d0db459b82397c2d2514fd84a9b74f7c3e
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-12-07 22:15:21 -0500

    Create InorderTraversal.cs

---
+++ LeetCodeSolutions/InorderTraversal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    class InorderTraversal
+    {
+        //public IList<int> InorderTraversal(TreeNode root)
+        //{
+        //    List<int> answers = new List<int>();
+        //    InorderTraversal(ref answers, root);
+        //    return answers;
+        //}
+
+        //private void InorderTraversal(ref List<int> answers, TreeNode root)
+        //{
+        //    if (root == null) return;
+
+        //    InorderTraversal(ref answers, root.left);
+        //    answers.Add(root.val);
+        //    InorderTraversal(ref answers, root.right);
+        //    return;
+        //}
+    }
+}
