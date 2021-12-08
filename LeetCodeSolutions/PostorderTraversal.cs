b2a6a5ebaf1a418cc7ee7f6420bd2ef6956542f3
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-12-07 22:15:26 -0500

    Create PostorderTraversal.cs

---
+++ LeetCodeSolutions/PostorderTraversal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    class PostorderTraversal
+    {
+        //public IList<int> PostorderTraversal(TreeNode root)
+        //{
+        //    List<int> answers = new List<int>();
+        //    PostorderTraversal(ref answers, root);
+        //    return answers;
+        //}
+
+        //private void PostorderTraversal(ref List<int> answers, TreeNode root)
+        //{
+        //    if (root == null) return;
+        //    PostorderTraversal(ref answers, root.left);
+        //    PostorderTraversal(ref answers, root.right);
+        //    answers.Add(root.val);
+        //    return;
+        //}
+    }
+}
