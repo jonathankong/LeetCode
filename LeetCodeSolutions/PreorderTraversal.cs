94f67aa6aac55efd948d438b67135b08550fbf6a
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-12-07 22:15:28 -0500

    Create PreorderTraversal.cs

---
+++ LeetCodeSolutions/PreorderTraversal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    class PreorderTraversal
+    {
+        //public IList<int> PreorderTraversal(TreeNode root)
+        //{
+        //    List<int> answers = new List<int>();
+        //    PreorderTraversal(ref answers, root);
+        //    return answers;
+        //}
+
+        //private void PreorderTraversal(ref List<int> answers, TreeNode root)
+        //{
+        //    if (root == null) return;
+        //    answers.Add(root.val);
+        //    PreorderTraversal(ref answers, root.left);
+        //    PreorderTraversal(ref answers, root.right);
+        //    return;
+        //}
+    }
+}
