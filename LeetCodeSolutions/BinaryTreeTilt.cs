192c95b14409ab4603819b6a1ca51837636708d8
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2021-12-07 22:15:19 -0500

    Create BinaryTreeTilt.cs

---
+++ LeetCodeSolutions/BinaryTreeTilt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    class BinaryTreeTilt
+    {
+        //int answer = 0;
+        //public int FindTilt(TreeNode root)
+        //{
+        //    if (root == null) return 0;
+        //    else return Math.Abs(SubTreeSum(root.left) - SubTreeSum(root.right)) + answer;
+        //}
+
+        //private int SubTreeSum(TreeNode root)
+        //{
+        //    if (root == null) return 0;
+
+        //    int leftTilt = SubTreeSum(root.left);
+        //    int rightTilt = SubTreeSum(root.right);
+
+        //    answer += Math.Abs(leftTilt - rightTilt);
+
+        //    return leftTilt + rightTilt + root.val;
+        //}
+    }
+}
