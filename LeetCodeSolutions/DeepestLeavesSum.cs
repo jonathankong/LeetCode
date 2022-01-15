a63f1975d460a747d69ea8655340a62e6ca595a7
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2022-01-14 19:35:59 -0500

    Update DeepestLeavesSum.cs

--- LeetCodeSolutions/DeepestLeavesSum.cs
+++ LeetCodeSolutions/DeepestLeavesSum.cs
@@ -6,5 +6,35 @@ namespace LeetCodeSolutions
 {
     class DeepestLeavesSum
     {
+        //int deepestNodeLevel = 0;
+        //int answer = 0;
+        //public int DeepestLeavesSum(TreeNode root)
+        //{
+        //    Recursion(root);
+        //    return answer;
+        //}
+
+        //private void Recursion(TreeNode root, int level = -1)
+        //{
+        //    if (root == null) return;
+        //    level += 1;
+        //    //This checks to see if we're at the end of one of the branches.
+        //    if (root.left == null && root.right == null)
+        //    {
+        //        if (level > deepestNodeLevel)
+        //        {
+        //            deepestNodeLevel = level;
+        //            answer = root.val;
+        //        }
+        //        else if (level == deepestNodeLevel)
+        //        {
+        //            answer += root.val;
+        //        }
+        //        return;
+        //    }
+
+        //    Recursion(root.left, level);
+        //    Recursion(root.right, level);
+        //}
     }
 }
