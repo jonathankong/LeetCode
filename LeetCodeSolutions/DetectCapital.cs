ea26db12e5c75a738671b6bc72cf1ec6cbfe7ed7
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2022-01-25 20:35:55 -0500

    Create DetectCapital.cs

---
+++ LeetCodeSolutions/DetectCapital.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    public static class DetectCapital
+    {
+        public static bool Solution(string word)
+        {
+            if (word.Length == 1) return true;
+
+            int index = 0;
+
+            if (char.IsUpper(word[index]))
+            {
+                index += 1;
+                if (char.IsUpper(word[index]))
+                {
+                    for (int i = index; i < word.Length; i++)
+                    {
+                        if (char.IsLower(word[i])) return false;
+                    }
+                }
+                else if (char.IsLower(word[index]))
+                {
+                    for (int i = index; i < word.Length; i++)
+                    {
+                        if (char.IsUpper(word[i])) return false;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = index; i < word.Length; i++)
+                {
+                    if (char.IsUpper(word[i])) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
