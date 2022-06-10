9fcaf32dd784613ebc17bfbbf57dbeba74bb428d
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2022-06-09 20:22:25 -0400

    Update InorderTraversal.cs
    Added iterative solution

--- LeetCodeSolutions/InorderTraversal.cs
+++ LeetCodeSolutions/InorderTraversal.cs
@@ -6,21 +6,48 @@ namespace LeetCodeSolutions
 {
     class InorderTraversal
     {
-        //public IList<int> InorderTraversal(TreeNode root)
-        //{
+        // Recursion
+        // public IList<int> InorderTraversal(TreeNode root)
+        // {
         //    List<int> answers = new List<int>();
         //    InorderTraversal(ref answers, root);
         //    return answers;
-        //}
+        // }
 
-        //private void InorderTraversal(ref List<int> answers, TreeNode root)
-        //{
+        // private void InorderTraversal(ref List<int> answers, TreeNode root)
+        // {
         //    if (root == null) return;
 
         //    InorderTraversal(ref answers, root.left);
         //    answers.Add(root.val);
         //    InorderTraversal(ref answers, root.right);
         //    return;
-        //}
+        // }
+
+        // Second Solution (Iterative)
+        // private void InorderTraversal(ref List<int> answers, TreeNode root)
+        // {
+        //     IList<int> answers = new List<int>();
+        //     if (root == null) return answers;
+            
+        //     Stack<TreeNode> treeStack = new Stack<TreeNode>();
+        //     while (true)
+        //     {
+        //         if (root != null)
+        //         {
+        //             treeStack.Push(root);
+        //             root = root.left;
+        //         }
+        //         else 
+        //         {
+        //             if (treeStack.Count == 0) break;
+        //             root = treeStack.Pop();
+        //             answers.Add(root.val);
+        //             root = root.right;
+        //         }
+        //     }
+
+        //     return answers;
+        // }
     }
 }
