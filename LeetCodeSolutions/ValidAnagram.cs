a83202a87428f0092a883c6f0e10009083208bb0
Jonathan Ong <jko665_000@msn.com>
2023-01-25 21:30:44 -0500

    Create ValidAnagram.cs
    2 working answers

---
+++ LeetCodeSolutions/ValidAnagram.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    static class ValidAnagram
+    {
+        public static bool Solution(string s, string t)
+        {
+            //First Solution with arrays
+            // char[] sArr = s.ToArray();
+            // char[] tArr = t.ToArray();
+
+            // Array.Sort(sArr);
+            // Array.Sort(tArr);
+
+            // return sArr.SequenceEqual(tArr);
+
+            //Second Solution with dictionaries check to see if text are same length
+            if (s.Length != t.Length) return false;
+
+            Dictionary<char, int> anagramDict = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                anagramDict.TryAdd(s[i], 0);
+                anagramDict.TryAdd(t[i], 0);
+                anagramDict[s[i]] += 1;
+                anagramDict[t[i]] -= 1;
+            }
+
+            foreach (KeyValuePair<char, int> kvp in anagramDict)
+            {
+                if (kvp.Value % 2 != 0) return false;
+            }
+            
+            return true;
+        }
+    }
+}
