112071ffc303f190bb6feccd854df97031202bfe
Jonathan Ong <jko665_000@msn.com>
2023-02-19 22:23:10 -0500

    Update ValidPalindrome.cs
    Using Regex
    Time Complexity o(n/2)

--- LeetCodeSolutions/ValidPalindrome.cs
+++ LeetCodeSolutions/ValidPalindrome.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace LeetCodeSolutions
 {
     class ValidPalindrome
     {
+        public static bool Solution(string s)
+        {
+            string newString = Regex.Replace(s, @"\s+", "");
+            
+            if (newString.Length < 2) return true;
+
+            int start = 0;
+            int end = newString.Length - 1;
+
+            while (end > start)
+            {
+                if (char.ToLower(newString[start]) == char.ToLower(newString[end]))
+                {
+                    start++;
+                    end--;
+                }
+                else return false;
+            }    
+
+            return true;
+        }
     }
 }
