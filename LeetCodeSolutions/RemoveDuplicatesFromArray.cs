73c2bbd68a1d29997e40fe8f66bc2546add32407
Jonathan Ong <jko665_000@msn.com>
2023-03-18 15:57:05 -0400

    Working first solution for RemoveDuplicatesFromArray
    Updated first solution;

--- LeetCodeSolutions/RemoveDuplicatesFromArray.cs
+++ LeetCodeSolutions/RemoveDuplicatesFromArray.cs
@@ -6,43 +6,60 @@ namespace LeetCodeSolutions
     {
         public static int Solution(int[] nums)
         {
+
+            //Updated first solution using 2 pointers: 
+            //One keeps track of unique numbers
+            //One iterates to find duplicates
+            //O(n) faster than second solution because no sorting but mem usage
+            // because of swapping 
             #region First Answer
-            // int currVal = nums[0];
-            // int swapIndex = 0;
-            // int numOfDuplicates = 0;
-            
-            // for (int counter = 1; counter < nums.Length; counter++)
-            // {
-            //     if (nums[counter] == currVal) 
-            //     {
-            //         if (swapIndex == 0) swapIndex = counter;
-            //         numOfDuplicates++;
-            //     }
-            //     else 
-            //     {
-            //         currVal = nums[counter];
-            //         nums[swapIndex] = currVal;
-            //         swapIndex++;
-            //     }
-            // }
-            // return numOfDuplicates;
-            #endregion
-            #region Second Answer
-            int currNum = nums[0];
-            int answer = 1;
-            int counter = 1;
-            while (counter < nums.Length)
+            if (nums.Length == 1) return 1;
+
+            int currNumIndx = 0;
+            int duplNumIndx = 1;
+            int ignoreNum = 101;
+
+            while (duplNumIndx < nums.Length)
             {
-                if (currNum == nums[counter]) nums[counter] = 101;
-                else if(currNum < nums[counter])
+                if (nums[currNumIndx] == nums[duplNumIndx])
                 {
-                    currNum = nums[counter];
-                    answer++;
+                    nums[duplNumIndx] = ignoreNum;
                 }
-                counter++;
+                else if (currNumIndx - duplNumIndx == 1)
+                {
+                    currNumIndx++;
+                }
+                else
+                {
+                    currNumIndx++;
+                    int tempNum = nums[duplNumIndx];
+                    nums[duplNumIndx] = nums[currNumIndx];
+                    nums[currNumIndx] = tempNum;
+                }
+                duplNumIndx++;
             }
-            Array.Sort(nums);
-            return answer;
+            return currNumIndx + 1;
+            #endregion
+            
+            //Iterate through array and change duplicates to large number
+            //Afterwards, sort array so duplicates go to the end
+            //O(n * nlogn)
+            #region Second Answer
+            // int currNum = nums[0];
+            // int answer = 1;
+            // int counter = 1;
+            // while (counter < nums.Length)
+            // {
+            //     if (currNum == nums[counter]) nums[counter] = 101;
+            //     else if(currNum < nums[counter])
+            //     {
+            //         currNum = nums[counter];
+            //         answer++;
+            //     }
+            //     counter++;
+            // }
+            // Array.Sort(nums);
+            // return answer;
             #endregion
         }
     }
