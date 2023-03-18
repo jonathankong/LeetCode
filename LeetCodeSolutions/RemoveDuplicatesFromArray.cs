24987d5576ae9e459341581a7f02bb20e3710d17
Jonathan Ong <jko665_000@msn.com>
2023-03-18 14:07:13 -0400

    Update RemoveDuplicatesFromArray.cs
    Added second solution; first solution seems to not be working; might review it later to fix it

--- LeetCodeSolutions/RemoveDuplicatesFromArray.cs
+++ LeetCodeSolutions/RemoveDuplicatesFromArray.cs
@@ -6,25 +6,44 @@ namespace LeetCodeSolutions
     {
         public static int Solution(int[] nums)
         {
-            int currVal = nums[0];
-            int swapIndex = 0;
-            int numOfDuplicates = 0;
+            #region First Answer
+            // int currVal = nums[0];
+            // int swapIndex = 0;
+            // int numOfDuplicates = 0;
             
-            for (int counter = 1; counter < nums.Length; counter++)
+            // for (int counter = 1; counter < nums.Length; counter++)
+            // {
+            //     if (nums[counter] == currVal) 
+            //     {
+            //         if (swapIndex == 0) swapIndex = counter;
+            //         numOfDuplicates++;
+            //     }
+            //     else 
+            //     {
+            //         currVal = nums[counter];
+            //         nums[swapIndex] = currVal;
+            //         swapIndex++;
+            //     }
+            // }
+            // return numOfDuplicates;
+            #endregion
+            #region Second Answer
+            int currNum = nums[0];
+            int answer = 1;
+            int counter = 1;
+            while (counter < nums.Length)
             {
-                if (nums[counter] == currVal) 
+                if (currNum == nums[counter]) nums[counter] = 101;
+                else if(currNum < nums[counter])
                 {
-                    if (swapIndex == 0) swapIndex = counter;
-                    numOfDuplicates++;
+                    currNum = nums[counter];
+                    answer++;
                 }
-                else 
-                {
-                    currVal = nums[counter];
-                    nums[swapIndex] = currVal;
-                    swapIndex++;
-                }
+                counter++;
             }
-            return numOfDuplicates;
+            Array.Sort(nums);
+            return answer;
+            #endregion
         }
     }
 }
