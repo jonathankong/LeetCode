d906634ebef1a956997155655ba9f09ceceb1bac
Jonathan Ong <jko665_000@msn.com>
2023-03-19 15:15:00 -0400

    Update FindPivotIndex.cs
    Second solution is much better as there's no nested loop

--- LeetCodeSolutions/FindPivotIndex.cs
+++ LeetCodeSolutions/FindPivotIndex.cs
@@ -11,21 +11,39 @@ namespace LeetCodeSolutions
         {
             //Used 2 pointers and nested loops
             #region First Solution
-            int left = 0;
-            int right = nums.Length - 1;
-            int leftSum = 0;
+            // int left = 0;
+            // int right = nums.Length - 1;
+            // int leftSum = 0;
 
-            for (left = 0; left < nums.Length; left++)
+            // for (left = 0; left < nums.Length; left++)
+            // {
+            //     leftSum += nums[left];
+            //     int rightSum = 0;
+            //     for (right = nums.Length - 1; right >= left; right--)
+            //     {
+            //         rightSum += nums[right];
+            //         if (right == left && rightSum == leftSum) return left;
+            //     }
+            // }
+
+            // return -1;
+            #endregion
+
+            //Get total sum and while calculating rolling sum from one side, 
+            //subtract from total sum until they equal
+            //This performed faster as there's no nested loop
+            #region Second Solution
+            int sum = nums.Sum();
+            int leftSum = 0;
+            int right = nums.Length - 1;
+
+            for (int left = 0; left < nums.Length; left++)
             {
                 leftSum += nums[left];
-                int rightSum = 0;
-                for (right = nums.Length - 1; right >= left; right--)
-                {
-                    rightSum += nums[right];
-                    if (right == left && rightSum == leftSum) return left;
-                }
+                if (leftSum == sum) return left;
+                sum -= nums[left];
+                right--;
             }
-
             return -1;
             #endregion
         }     
