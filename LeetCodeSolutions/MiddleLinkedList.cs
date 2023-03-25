a56180e1028333ea4c32f7c42c13aa7ff3dcd3cf
Jonathan Ong <jko665_000@msn.com>
2023-03-25 11:23:13 -0400

    Update MiddleLinkedList.cs
    Added second solution

--- LeetCodeSolutions/MiddleLinkedList.cs
+++ LeetCodeSolutions/MiddleLinkedList.cs
@@ -6,24 +6,42 @@ namespace LeetCodeSolutions
     {
         public static ListNode Solution(ListNode head)
         {
-            //If there's only one node
-            if (head.next == null) return head;
+            #region First Answer
+            // //If there's only one node
+            // if (head.next == null) return head;
             
-            int nodeCounter = 1;
+            // int nodeCounter = 1;
             
-            ListNode answer = head;
+            // ListNode answer = head;
+            // ListNode currNode = head;
+            
+            // while (currNode != null)
+            // {
+            //     //If the current node counter is even, that means we need to move the answer up the linked list
+            //     if (nodeCounter % 2 == 0) answer = answer.next;
+                
+            //     currNode = currNode.next;
+            //     nodeCounter += 1;
+            // }
+            
+            // return answer;
+            #endregion
+
+            //Cleaner solution after coming back to answer it after a year
+            #region Second Answer
+            int count = 0;
+            ListNode midNode = head;
             ListNode currNode = head;
-            
+
             while (currNode != null)
             {
-                //If the current node counter is even, that means we need to move the answer up the linked list
-                if (nodeCounter % 2 == 0) answer = answer.next;
-                
+                count++;
+                if (count % 2 == 0) midNode = midNode.next;
                 currNode = currNode.next;
-                nodeCounter += 1;
+
             }
-            
-            return answer;
+            return midNode;
+            #endregion
         }
     }
 }
